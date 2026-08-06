b48f851365b2b6dff7ba8cfe4e421c6508fd2b43
Javier Cordova <javiercordova@users.noreply.github.com>
2026-08-06 14:41:40 -0400

    Clean deploy: web assets only (optim ce493dc)

---
+++ assets/index-CCO2k1bS.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BiRg3lly.js","assets/index-C9vkV06k.js","assets/index-DD0n44nf.css"])))=>i.map(i=>d[i]);
+import{r,_ as a}from"./index-C9vkV06k.js";const _=r("Calendar",{web:()=>a(()=>import("./web-BiRg3lly.js"),__vite__mapDeps([0,1,2])).then(e=>new e.CalendarWeb)});export{_ as Calendar};
